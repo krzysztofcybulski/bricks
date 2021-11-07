eb151d95585f6c5c2fee5d67a80c0b0d8e73acf9
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2021-11-07 18:37:30 +0100

    Move reading json to IO dispatcher

--- server/src/main/kotlin/me/kcybulski/bricks/server/api/WSHandler.kt
+++ server/src/main/kotlin/me/kcybulski/bricks/server/api/WSHandler.kt
@@ -3,13 +3,16 @@ package me.kcybulski.bricks.server.api
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.kotest.common.runBlocking
+import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.withContext
 import me.kcybulski.bricks.server.lobby.OpenLobby
 import me.kcybulski.bricks.web.ImHealthy
 import me.kcybulski.bricks.web.MoveMessage
 import me.kcybulski.bricks.web.ReadyMessage
 import me.kcybulski.bricks.web.RegisterMessage
 import me.kcybulski.bricks.web.UserMessage
+import mu.KotlinLogging
 import ratpack.websocket.WebSocket
 import ratpack.websocket.WebSocketClose
 import ratpack.websocket.WebSocketHandler
@@ -20,12 +23,22 @@ class WSHandler(
     private val objectMapper: ObjectMapper = jacksonObjectMapper()
 ) : WebSocketHandler<String> {
 
+    private val logger = KotlinLogging.logger {}
+
     override fun onOpen(webSocket: WebSocket): String? = null
 
     override fun onClose(close: WebSocketClose<String>) {}
 
-    override fun onMessage(frame: WebSocketMessage<String>) {
-        runBlocking { handleMessage(objectMapper.readValue(frame.text, UserMessage::class.java), frame.connection) }
+    override fun onMessage(frame: WebSocketMessage<String>): Unit = runBlocking {
+        parseUserMessage(frame)
+            .onSuccess { handleMessage(it, frame.connection) }
+            .onFailure { logger.warn(it) { "Ignoring user message ${frame.text}" } }
+    }
+
+    private suspend fun parseUserMessage(frame: WebSocketMessage<String>) = withContext(IO) {
+        runCatching {
+            objectMapper.readValue(frame.text, UserMessage::class.java)
+        }
     }
 
     private suspend fun handleMessage(message: UserMessage, connection: WebSocket) = coroutineScope {
