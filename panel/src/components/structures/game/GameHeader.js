619266a03272c92539fdded392ee4127cb027af3
Krzysztof Cybulski <krzysztof.cybulski@allegro.pl>
2022-06-14 11:34:33 +0200

    Add color bar

--- panel/src/components/structures/game/GameHeader.js
+++ panel/src/components/structures/game/GameHeader.js
@@ -3,25 +3,29 @@ import Avatar from '../../atoms/Avatar';
 import Text from '../../atoms/Text';
 import { Box, Stack } from 'grommet';
 import { Trophy } from 'grommet-icons';
+import VerticalBox from "../../atoms/VerticalBox";
 
 const GameHeader = ({ players: { first, second } }) =>
     <HorizontalBox justify="between" align="center" pad="medium">
-        <Player player={first} />
+        <Player player={first}/>
         <Text>vs</Text>
-        <Player player={second} />
+        <Player player={second}/>
     </HorizontalBox>;
 
-const Player = ({ player: { image, winner } }) =>
-    <Stack align="stretch">
-        <Avatar url={image} size="large"/>
-        {winner &&
-            <Box justify="end" align="center"
-                 style={{ position: 'relative', top: 12 }}
-                 height="100%" width="100%">
-                <Trophy color="#fffff"/>
-            </Box>
-        }
-    </Stack>;
+const Player = ({ player: { image, color, winner } }) =>
+    <VerticalBox align="stretch">
+        <Stack align="stretch">
+            <Avatar url={image} size="large"/>
+            {winner &&
+                <Box justify="end" align="center"
+                     style={{ position: 'relative', top: 12 }}
+                     height="100%" width="100%">
+                    <Trophy/>
+                </Box>
+            }
+        </Stack>
+        <Box background={{ color }} height='small'/>
+    </VerticalBox>;
 
 export default GameHeader;
 
